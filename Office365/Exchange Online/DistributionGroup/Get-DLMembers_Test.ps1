1849d5e61795e550a16a53f129507611a2660547
Young Ryu <Young.Ryu@outlook.com>
2017-06-16 13:08:33 -0700

    Add files via upload

---
+++ Office365/Exchange Online/DistributionGroup/Get-DLMembers_Test.ps1
@@ -0,0 +1,20 @@
+﻿#$Name = Get-DistributionGroup -ResultSize Unlimited -Identity "IT-O365" | Select Name
+$Name = Get-DistributionGroup -ResultSize Unlimited
+write-host $Name
+#Wirte-host $Name
+(Get-DistributionGroupMember $Name).Count
+#(Get-DistributionGroupMember -ResultSize unlimited "hq").Count
+
+
+#$group = Get-DistributionGroup –identity "Team-Sales" | Select Name 
+#(Get-DistributionGroupMember $group).Count
+
+
+(Get-DistributionGroupMember –identity "Apac-Se-Leads").Count
+
+
+$GN = gc d:\distros.txt
+foreach($name in $GN) {
+    $Count = (Get-DistributionGroupMember -Identity $name | select name).count
+    Write-host -ForegroundColor Cyan "$name has $Count members"
+}
---
+++ Office365/Exchange Online/DistributionGroup/List of Get-DistributionGroups.ps1
@@ -0,0 +1,21 @@
+﻿################################################################################################################################################################ 
+# Script Get DistributionGroup in Office 365
+# DL Name | Owner(s) | Allow External Senders | DL Group ID
+# 
+# 
+# To run the script : Need "Global Admin" permission in Office365
+# 
+# 
+# 
+# Author:                 Young Ryu
+# Version:                1.1 
+# Last Modified Date:     5/25/2017 
+# Last Modified By:       Young Ryu (Young.Ryu@nutanix.com) 
+################################################################################################################################################################ 
+
+
+$Creds = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+
+Get-Distributiongroup -ResultSize Unlimited | Sort -Property RequireSenderAuthenticatioNEnabled | Select Name, Managedby, RequireSenderAuthenticationEnabled, PrimarySMTPAddress | Export-CSV "C:\Users\Young.Ryu.YRYUSJC-PC2\Nutanix\OneDrive - Nutanix\Application Services\O365\PS_Output\Get-ListOfDL_Managers_DeliveryManagement.csv"

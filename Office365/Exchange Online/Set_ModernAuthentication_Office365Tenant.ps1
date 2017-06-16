a574cd5557d2812d88e29c20408e0af6c99eb974
YoungRyuCode <Young.Ryu@outlook.com>
2017-06-16 12:51:58 -0700

    Add files via upload

---
+++ Office365/Exchange Online/Set_ModernAuthentication_Office365Tenant.ps1
@@ -0,0 +1,13 @@
+﻿#Connect Exchange Online Service
+$Creds = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+
+#Set Modern Authentication into Nutanixinc.onmicrosoft.com tenant
+Set-OrganizationConfig -OAuth2ClientProfileEnabled $true
+
+#Validate Nutanixinc.onmicrosoft.com = true 
+Get-OrganizationConfig | Format-Table -Auto Name,OAuth*
+
+#Disconnect Exchange Online service
+Remove-PSSession $Session
